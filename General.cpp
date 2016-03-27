db9108d69783d0a9eba4f6ecd00533daf795f40f
Justin B <bonnerj@purdue.edu>
2016-03-27 17:45:55 -0400

    Began implimentation of the General class

--- General.cpp
+++ General.cpp
@@ -1,10 +1,54 @@
 #include "General.h"
 
+#include <iostream>
 
-General::General() {
-    
+General::General(Bot* bot) {
+    m_bot = bot;
 }
 
 General::~General() {
     
+}
+
+
+void General::getAttack(){
+    
+}
+
+
+void General::getDefense(){
+    
+}
+
+
+void General::calculateStrategy(){
+    
+}
+
+
+void General::placeReinforcements(int numReinforcements){
+    float sumWeights = 0;
+    for(SuggestedMove* sugRein : m_suggestedReinforcements){
+        sumWeights += sugRein->percentageMove;
+    }
+    if(sumWeights == 0){
+         std::cerr << "\nERROR NO SUGGESTED MOVES IN FUNCTION 'placeReinforcements' in General.cpp\n";
+         return;
+    }
+    
+    for(SuggestedMove* sugRein : m_suggestedReinforcements){
+        m_bot->addReinforcements(sugRein->to, sugRein->percentageMove * numReinforcements / sumWeights);
+    }
+}
+
+
+void General::makeMoves(){
+    
+    
+    
+    //clear suggestion arrays for next turn
+    m_suggestedAttacks.clear();
+    m_suggestedDefenses.clear();
+    m_suggestedReinforcements.clear();
+    m_plannedMoves.clear();
 }
--- General.h
+++ General.h
@@ -1,7 +1,13 @@
 #ifndef GENERAL_H
 #define GENERAL_H
 
-struct suggested_Move {
+#include <vector>
+
+#include "Bot.h"
+#include "region.h"
+#include "SuperRegion.h"
+
+typedef struct suggested_Move {
     Region* from;
     Region* to;
     float percentageMove; //a float who's value is from 0 to 1, representing the percent of available troops to move, always leaving 1.
@@ -11,9 +17,22 @@ class General {
     
     public:
         
-        General();
+        General(Bot* bot);
         
         virtual ~General();
+        
+        
+        /** \brief Calculates the suggested attacks for this turn.
+         *
+         *
+         */                          
+        void getAttack();
+        
+        /** \brief Calculates the suggested defensive moves for this turn.
+         * a suggested move can be to keep troops in a region.
+         *
+         */                          
+        void getDefense();
         
         /** calculateStrategy
          *  
@@ -21,14 +40,14 @@ class General {
          *  prioritizes different attacks and moves taking into account the
          *  results from getAttack and getDefense.
          */
-        void calculateStrategy();
+        void calculateStrategy(); //will clear suggested attacks and suggested defences
         
-        /** placeReinforcements
-         *  
-         *  This function places troops during the add reinforcements phase
-         *  
-         */
-        void placeReinforcements(); //may need additional parameters later
+        /** \brief This function places the number of reinforcements according the weights calculated by the general.
+         *
+         * \param numReinforcements int the number of reinforcements available to add
+         * \return void
+         */                                            
+        void placeReinforcements(int numReinforcements); //may need additional parameters later
         
         /** makeMoves
          *  
@@ -36,16 +55,24 @@ class General {
          *  phase.
          *  
          */
-        void makeMoves();
+        void makeMoves(); //calling this function will clear m_suggestedMoves
 
     protected:
         
 
     private:
         
-        //will also need a data structure for storing suggested moves
+        std::vector<SuggestedMove*> m_suggestedAttacks;
+        
+        std::vector<SuggestedMove*> m_suggestedDefenses; //suggested moves with to and from as the same region
+        
+        std::vector<SuggestedMove*> m_suggestedReinforcements; //suggested moves with from region as null
+        
+        std::vector<SuggestedMove*> m_plannedMoves;
         
         Map* gameMap;
+        
+        Bot* m_bot;
         
 };
 
