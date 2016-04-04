cac6b729afb69f1f7034a9b2b60c015a5dc5f7b9
Erik V <eski787@gmail.com>
2016-04-03 21:05:52 -0400

    Minor changes

--- Map.cpp
+++ Map.cpp
@@ -1,26 +1,24 @@
 #include "Map.h"
 
-Map::Map()
-{
+Map::Map() {
 }
 
-Map::Map(Map & copy)
-{
+Map::Map(Map & copy) {
+	Map nmap;
+	nmap.dirty = dirty;
+	nmap.regions = regions;
+	nmap.superRegions = superRegions;
 }
 
-void Map::addSuperRegion(unsigned id, int reward)
-{
-	while (superRegions.size() <= id)
-	{
+void Map::addSuperRegion(unsigned id, int reward) {
+	while (superRegions.size() <= id) {
 		superRegions.push_back(SuperRegion());
 	}
 	superRegions[id] = SuperRegion(reward);
 }
 
-void Map::addRegion(unsigned id, int super)
-{
-	while (regions.size() <= id)
-	{
+void Map::addRegion(unsigned id, int super) {
+	while (regions.size() <= id) {
 		regions.push_back(Region());
 	}
 
@@ -28,33 +26,28 @@ void Map::addRegion(unsigned id, int super)
 	getSuperRegion(super).addRegion(&regions[id]);
 }
 
-void Map::setNeighbors(unsigned id1, unsigned id2)
-{
+void Map::setNeighbors(unsigned id1, unsigned id2) {
 	regions[id1].addNeighbor(&regions[id2]);
 	regions[id2].addNeighbor(&regions[id1]);
 }
 
-void Map::setWasteland(unsigned id)
-{
+void Map::setWasteland(unsigned id) {
 }
 
-void Map::updateRegion(const unsigned& id, const Player& player, const int& armies)
-{
+void Map::updateRegion(const unsigned& id, const Player& player, const int& armies) {
+	dirty = true;
 	regions[id].setArmies(armies);
 	regions[id].setOwner(player);
 }
 
-Region & Map::getRegion(int id)
-{
+Region & Map::getRegion(int id) {
 	return regions.at(id);
 }
 
-SuperRegion & Map::getSuperRegion(int id)
-{
+SuperRegion & Map::getSuperRegion(int id) {
 	return superRegions.at(id);
 }
 
-bool Map::isDirty()
-{
+bool Map::isDirty() {
 	return dirty;
 }
